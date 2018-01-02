cf2eae406adbed99fe380f1e3ea49be37bc95f24
Finn Pedersen <fpeders2@orange.fr>
2018-01-02 10:39:05 +0100

    got it to work again

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -4,11 +4,6 @@
     Type in your message:
     <input type="text" id="input1" v-model="message" size="50">
     <p>The value of the input is: {{ message }}</p>
-    <ul>
-      <li v-for="name in names" v-text="name"></li>
-    </ul>
-    <input type="text" id="input2">
-    <button v-on:click="onClick ">Add Name</button>
   </div>
 </template>
 
@@ -16,8 +11,7 @@
   export default {
     data: function () {
       return {
-        message: "Welcome to Cinematronix!",
-        names: ['Joe', 'Mary', 'Jane', 'Jack ']
+        message: "Welcome to Cinematronix!"
       }
     }
   }
